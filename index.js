dbb14b0c3846e495bfca6503cef9eb958b3ae5e8
corkesw <home@stephencorke.com>
2021-10-04 15:12:34 +0100

    add  question generation

--- index.js
+++ index.js
@@ -1,30 +1,55 @@
-// function wrongAnswer(id) {
-//     const result = document.getElementsByClassName('unanswered_class')
-// console.log(...result)
-//     // result.classList.add('incorrect_class')
-//     // result.classList.remove('unanswered_class')
+let correctAnswerCount = 0;
+let questionAnswered = false;
 
-//     //console.log(result.classList)
+const questions = {
+      question: "What country is associated with Roast Beef and Yorkshire Pudding?",
+      answer: [
+          "Mexico", "France", "England", "Guatemala"
+      ],
+      correctAnswer: 2
+}
 
-// }
+function questionMaker() {
+    const question = document.getElementById('question_insert')
+    const answer0 = document.getElementById('answer0')
+    answer0.innerText = questions.answer[0]
+    const answer1 = document.getElementById('answer1')
+    answer1.innerText = questions.answer[1]
+    const answer2 = document.getElementById('answer2')
+    answer2.innerText = questions.answer[2]
+    const answer3 = document.getElementById('answer3')
+    answer3.innerText = questions.answer[3]
+    question.innerText = questions.question
 
-// function correctAnswer(id) {
-//     const result = document.getElementById(id)
-//     result.classList.add('correct_class')
-//     result.classList.remove("unanswered_class")
+    const correctAnswer = `answer${questions.correctAnswer}`
+    const correctAnswerHTML = document.getElementById(correctAnswer)
+    correctAnswerHTML.className = 'unanswered_class_correct'
+    correctAnswerHTML.setAttribute('onclick', 'answerQuestion(true)')
 
+}
 
-function answerQuestion() {
-    const incorrectResult = [...document.getElementsByClassName('unanswered_class_wrong')]
-    incorrectResult.forEach(result => result.className = 'incorrect_class')
+function answerQuestion(answer) {
+  const incorrectResult = [
+    ...document.getElementsByClassName("unanswered_class_wrong"),
+  ];
+  incorrectResult.forEach((result) => (result.className = "incorrect_class"));
 
-    const correctResult = [...document.getElementsByClassName('unanswered_class_correct')]
-    correctResult.forEach(result => result.className = 'correct_class')
+  const correctResult = [
+    ...document.getElementsByClassName("unanswered_class_correct"),
+  ];
+  correctResult.forEach((result) => (result.className = "correct_class"));
 
+  const answer_text = document.getElementById("answer_text");
 
-    console.log(incorrectResult[0].className)
+  if (answer) {
+    answer_text.innerText = "That's correct!";
+    if ((questionAnswered === false)) {
+      correctAnswerCount++;
+      questionAnswered = true;
+    }
 
-
-    //const correctResult = document.getElementsByClassName('unanswered_class_correct')
-
+    console.log(correctAnswerCount);
+  } else {
+    answer_text.innerText = "Wrong, loser!";
+  }
 }
