7c04b40edddd207b62888aaa546678f0ec4a9912
BulleyBulley <philbulleyment@yahoo.co.uk>
2021-10-04 17:07:20 +0100

    adds styling

--- index.js
+++ index.js
@@ -1,82 +1,71 @@
 let correctAnswerCount = 0;
 let questionAnswered = false;
 
-
-
 const bankOfQuestions = [
-    {
-        question: "What country is associated with Roast Beef and Yorkshire Pudding?",
-        answer: [
-            "Mexico", "France", "England", "Guatemala"
-        ],
-        correctAnswer: 2
+  {
+    question:
+      "What country is associated with Roast Beef and Yorkshire Pudding?",
+    answer: ["Mexico", "France", "England", "Guatemala"],
+    correctAnswer: 2,
   },
   {
     question: "Where is the home of the massaman curry?",
-    answer: [
-        "Thailand", "Malaysia", "Japan", "Wales"
-    ],
-    correctAnswer: 0
-},
-{
+    answer: ["Thailand", "Malaysia", "Japan", "Wales"],
+    correctAnswer: 0,
+  },
+  {
     question: "Where does Kiwi fruit originate?",
-    answer: [
-        "Portugal", "New Zealand", "Australia", "Singapore"
-    ],
-    correctAnswer: 1
-}
-]
+    answer: ["Portugal", "New Zealand", "Australia", "Singapore"],
+    correctAnswer: 1,
+  },
+];
 
-const questionNumbers = [0 ,1 , 2]
-questionNumbers.sort(() => Math.random() - 0.5)
+const questionNumbers = [0, 1, 2];
+questionNumbers.sort(() => Math.random() - 0.5);
 questions = questionNumbers.map((number) => {
-    return bankOfQuestions[number]
-})
+  return bankOfQuestions[number];
+});
 
 function questionMaker() {
-    const resetWrongResult = [
-        ...document.getElementsByClassName("incorrect_class"),
-      ];
-    resetWrongResult.forEach(function (result){
-        result.className = "unanswered_class_wrong"
-        result.setAttribute('onclick', 'answerQuestion(false)')
-    }) ;
+  const resetWrongResult = [
+    ...document.getElementsByClassName("incorrect_class"),
+  ];
+  resetWrongResult.forEach(function (result) {
+    result.className = "unanswered_class_wrong";
+    result.setAttribute("onclick", "answerQuestion(false)");
+  });
 
-    const resetCorrectResult = [
-        ...document.getElementsByClassName("correct_class"),
-      ];
-      resetCorrectResult.forEach(function (result){
-          result.className = "unanswered_class_wrong"
-          result.setAttribute('onclick', 'answerQuestion(false)')
-      })
+  const resetCorrectResult = [
+    ...document.getElementsByClassName("correct_class"),
+  ];
+  resetCorrectResult.forEach(function (result) {
+    result.className = "unanswered_class_wrong";
+    result.setAttribute("onclick", "answerQuestion(false)");
+  });
 
+  questionAnswered = false;
+  document.getElementById("answer_text").innerText = "";
 
-     questionAnswered = false
-     document.getElementById('answer_text').innerText = "";
-     
-     
-    
-      const question = document.getElementById('question_insert')
-    const answer0 = document.getElementById('answer0')
-    answer0.innerText = questions[0].answer[0]
-    const answer1 = document.getElementById('answer1')
-    answer1.innerText = questions[0].answer[1]
-    const answer2 = document.getElementById('answer2')
-    answer2.innerText = questions[0].answer[2]
-    const answer3 = document.getElementById('answer3')
-    answer3.innerText = questions[0].answer[3]
-    question.innerText = questions[0].question
+  const question = document.getElementById("question_insert");
+  const answer0 = document.getElementById("answer0");
+  answer0.innerText = questions[0].answer[0];
+  const answer1 = document.getElementById("answer1");
+  answer1.innerText = questions[0].answer[1];
+  const answer2 = document.getElementById("answer2");
+  answer2.innerText = questions[0].answer[2];
+  const answer3 = document.getElementById("answer3");
+  answer3.innerText = questions[0].answer[3];
+  question.innerText = questions[0].question;
 
-    const correctAnswer = `answer${questions[0].correctAnswer}`
-    const correctAnswerHTML = document.getElementById(correctAnswer)
-    correctAnswerHTML.className = 'unanswered_class_correct'
-    correctAnswerHTML.setAttribute('onclick', 'answerQuestion(true)')
-    questions.shift()
-    if (questions.length === 0) { 
-        document.getElementById('next_id').setAttribute('onclick', 'getResults()')
-        document.getElementById('next_id').innerText = ('Get Results')
-
-    }
+  const correctAnswer = `answer${questions[0].correctAnswer}`;
+  const correctAnswerHTML = document.getElementById(correctAnswer);
+  correctAnswerHTML.className = "unanswered_class_correct";
+  correctAnswerHTML.setAttribute("onclick", "answerQuestion(true)");
+  questions.shift();
+  if (questions.length === 0) {
+    document.getElementById("next_id").setAttribute("onclick", "getResults()");
+    document.getElementById("next_id").innerText = "Get Results";
+  }
 }
 
 function answerQuestion(answer) {
@@ -94,18 +83,20 @@ function answerQuestion(answer) {
 
   if (answer) {
     answer_text.innerText = "That's correct!";
-    if ((questionAnswered === false)) {
+    if (questionAnswered === false) {
       correctAnswerCount++;
-      
+
       questionAnswered = true;
     }
   } else {
-    questionAnswered = true;  
+    questionAnswered = true;
     answer_text.innerText = "Wrong, loser!";
   }
 }
 
 function getResults() {
-    console.log("I am called")
-    document.getElementById('final_score').innerText = (`You have scored ${correctAnswerCount}`)
+  console.log("I am called");
+  document.getElementById(
+    "final_score"
+  ).innerText = `You have scored ${correctAnswerCount}`;
 }
