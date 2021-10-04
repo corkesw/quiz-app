5022ee2338f92aecd4d2e2b9a513d61b414c9751
corkesw <home@stephencorke.com>
2021-10-04 15:38:22 +0100

    add random question generater

--- index.js
+++ index.js
@@ -1,27 +1,52 @@
 let correctAnswerCount = 0;
 let questionAnswered = false;
 
-const questions = {
-      question: "What country is associated with Roast Beef and Yorkshire Pudding?",
-      answer: [
-          "Mexico", "France", "England", "Guatemala"
-      ],
-      correctAnswer: 2
+
+
+const bankOfQuestions = [
+    {
+        question: "What country is associated with Roast Beef and Yorkshire Pudding?",
+        answer: [
+            "Mexico", "France", "England", "Guatemala"
+        ],
+        correctAnswer: 2
+  },
+  {
+    question: "Where is the home of the massaman curry?",
+    answer: [
+        "Thailand", "Malaysia", "Japan", "Wales"
+    ],
+    correctAnswer: 0
+},
+{
+    question: "Where does Kiwi fruit originate?",
+    answer: [
+        "Portugal", "New Zealand", "Australia", "Singapore"
+    ],
+    correctAnswer: 1
 }
+]
+
+const questionNumbers = [0 ,1 , 2]
+questionNumbers.sort(() => Math.random() - 0.5)
+questions = questionNumbers.map((number) => {
+    return bankOfQuestions[number]
+})
+console.log(questions, '<<<<<<<<<')
 
 function questionMaker() {
     const question = document.getElementById('question_insert')
     const answer0 = document.getElementById('answer0')
-    answer0.innerText = questions.answer[0]
+    answer0.innerText = questions[0].answer[0]
     const answer1 = document.getElementById('answer1')
-    answer1.innerText = questions.answer[1]
+    answer1.innerText = questions[0].answer[1]
     const answer2 = document.getElementById('answer2')
-    answer2.innerText = questions.answer[2]
+    answer2.innerText = questions[0].answer[2]
     const answer3 = document.getElementById('answer3')
-    answer3.innerText = questions.answer[3]
-    question.innerText = questions.question
+    answer3.innerText = questions[0].answer[3]
+    question.innerText = questions[0].question
 
-    const correctAnswer = `answer${questions.correctAnswer}`
+    const correctAnswer = `answer${questions[0].correctAnswer}`
     const correctAnswerHTML = document.getElementById(correctAnswer)
     correctAnswerHTML.className = 'unanswered_class_correct'
     correctAnswerHTML.setAttribute('onclick', 'answerQuestion(true)')
